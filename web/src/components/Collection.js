853960425c2c011729af73d9e121054d87768859
Alex Soroka <lehaSVV2009@gmail.com>
2017-10-22 20:49:30 +0300

    Remove search input cause hard to implement

--- web/src/components/Collection.js
+++ web/src/components/Collection.js
@@ -1,34 +1,26 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
+import Avatar from 'material-ui/Avatar'
+import Chip from 'material-ui/Chip'
 import Subheader from 'material-ui/List/ListSubheader'
-import TextField from 'material-ui/TextField'
 import { Trans } from 'react-i18next'
 
-import CollectionItem from './CollectionItem'
-
-export default class Collection extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      search: '',
-      items: this.props.items
-    }
+const styles = theme => ({
+  chip: {
+    margin: theme.spacing.unit,
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap'
   }
+})
 
-  handleSearchTextChange = (event) => {
-    const text = event.target.value
-    this.setState({
-      search: text,
-      items: !text ? this.props.items : this.props.items.filter(
-        item => this.containsSearchText(item.name, text) || this.containsSearchText(item.value + '', text))
-    })
-  }
-
-  containsSearchText = (text, searchText) => {
-    return text && searchText && text.toLowerCase().includes(searchText.toLowerCase())
-  }
+class Collection extends Component {
 
   render() {
-    const { items } = this.state;
+    const { classes, items } = this.props;
 
     if (!Array.isArray(items)) {
       return (<Trans>Список пустой</Trans>)
@@ -37,21 +29,25 @@ export default class Collection extends Component {
     return (
       <div>
         <Subheader>{this.props.name}</Subheader>
-        <TextField
-          label={<Trans>Поиск</Trans>}
-          value={this.state.search}
-          onChange={this.handleSearchTextChange}
-        />
-        {
-          items.map((item, index) => (
-            <CollectionItem
-              key={index}
-              name={item.name}
-              value={item.value}
-            />
-          ))
-        }
+        <div className={classes.row}>
+          {
+            items.map((item, index) => (
+              <Chip
+                key={index}
+                className={classes.chip}
+                avatar={<Avatar>{'' + item.value}</Avatar>}
+                label={item.name}
+              />
+            ))
+          }
+        </div>
       </div>
     )
   }
 }
+
+Collection.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Collection)
