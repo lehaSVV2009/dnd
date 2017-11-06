cc1ff13755e41160396fb91f81db75b2bd47756e
Alex Soroka <lehaSVV2009@gmail.com>
2017-11-06 17:44:51 +0300

    No duplicates in grid

--- web/src/containers/MainPage.js
+++ web/src/containers/MainPage.js
@@ -2,27 +2,93 @@ import React, { Component } from 'react'
 import Grid from 'material-ui/Grid'
 import { Trans } from 'react-i18next'
 
+import * as api from '../API'
 import DayPage from './DayPage'
-import HeroPage from './HeroPage'
+import Condition from '../components/Condition'
+import Profile from '../components/Profile'
+import SkillsList from '../components/SkillsList'
+import TalentsList from '../components/TalentsList'
 
 export default class MainPage extends Component {
-  render() {
-    const { heroId } = this.props
+  constructor(props) {
+    super(props)
+    this.state = {
+      heroId: props.heroId,
+      hero: null,
+      heroLoading: false,
+      error: null
+    }
+  }
 
-    if (!heroId) {
+  /**
+   * Load when component is created.
+   */
+  componentDidMount = () => {
+    this.setState({ hero: null, heroLoading: true, error: null })
+    this.handleHeroResponse(
+      api.fetchHero({ id: this.state.heroId })
+    )
+  }
+
+  handleHeroChange = (newJson) => {
+    this.handleHeroResponse(
+      api.patchHero({ id: this.state.heroId, hero: newJson })      
+    )
+  }
+
+  handleHeroResponse = (promise) => {
+    promise
+      .then((response) => {
+        this.setState({ hero: response.data, heroLoading: false, error: null })
+      })
+      .catch((error) => {
+        this.setState({ hero: null, heroLoading: false, error: error.toString() })            
+      })
+  }
+
+  render() {
+    const { hero, heroLoading, error } = this.state
+    
+    // Show loading bar if HTTP request is not completed
+    if (heroLoading) {
+      return (<Trans>Загрузка</Trans>)
+    }
+
+    // Show error if HTTP request failed
+    if (error) {
+      return (<div>{error}</div>)
+    }
+
+    if (!hero) {
       return (<Trans>Герой отсутствует</Trans>)
     }
 
     return (
       <Grid container spacing={24}>
         <Grid item xs={12} md={4} lg={3}>
-          <HeroPage heroId={heroId}/>
+          <Profile
+            profile={hero.profile}
+            onChange={this.handleHeroChange}
+          />
+          <br/>
+          <SkillsList
+            characteristics={hero.characteristics}
+            skills={hero.skills}
+          />
         </Grid>
         <Grid item xs={12} md={4} lg={6}>
-          <DayPage heroId={heroId}/>
+          <DayPage heroId={hero.id}/>
         </Grid>
         <Grid item xs={12} md={4} lg={3}>
-          <HeroPage heroId={heroId}/>
+          <Condition 
+            condition={hero.condition}
+            protections={hero.protections}
+            onChange={this.handleHeroChange}
+          />
+          <br/>
+          <TalentsList
+            talents={hero._embedded.talents}
+          />
         </Grid>
       </Grid>
     )
